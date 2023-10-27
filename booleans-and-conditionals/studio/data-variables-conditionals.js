e5b6425eced93402c42070e95f35c1c77cecf4d3
Meka <ccanyanetu@gmail.com>
2023-10-27 17:32:21 -0400

    lime and pink

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,35 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18"
+
+let time = "10:05:34 AM"
+// add logic below to verify all astronauts are ready
+let astronautCount = 7
+// add logic below to verify the total mass does not exceed the maximum limit of 850000
+let astronautStatus = "ready"
+// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg
+// add logic below to verify the fuel level is at 100%
+let fuelMassKg = 760,000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = 100%
+// add logic below to verify the weather status is clear
+let weatherStatus = clear
+// Verify shuttle launch can proceed based on above conditions
+let preparedForLiftOff = true
+if (astronautCount <=7) {
+    preparedForLiftOff = false
+}
+
+console.log(preparedForLiftOff)
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
 // add logic below to verify all astronauts are ready
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
-
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
-// add logic below to verify the fuel level is at 100%
-
-// add logic below to verify the weather status is clear
-
-// Verify shuttle launch can proceed based on above conditions
